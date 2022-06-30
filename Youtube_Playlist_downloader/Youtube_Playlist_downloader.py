c7c3591797bf21716ecade8e995c27fbbef794c9
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-06-30 22:37:00 +0500

    Add files via upload
    Initial commit

---
+++ Youtube_Playlist_downloader/Youtube_Playlist_downloader.py
@@ -0,0 +1,14 @@
+from pytube import Playlist
+link=input("Enter Playlist Link ")
+pt=Playlist(link)
+print("---------------------------------------------------------------")
+print(f'Downloading: {pt.title}')
+print("---------------------------------------------------------------")
+s=1;
+for video in pt.videos:
+    print("---------------------------------------------------------------")
+    print(f'{s}: {video.title}')
+    s=s+1;
+    print("---------------------------------------------------------------")
+    video.streams.filter(res="360p").first().download()
+print("Download Ending")
